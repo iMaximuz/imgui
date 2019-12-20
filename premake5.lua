54fb876c217f1f67c15d49b1f9b7b31cebb23d23
Luis Almaguer <imaximuz@gmail.com>
2019-12-20 14:24:22 -0600

    premake file compatible

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_testedit.h",
+        "imstb_truetype.h",
+        "imgui_demo"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+    
+    filter "system:macosx"
+        staticruntime "On"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
